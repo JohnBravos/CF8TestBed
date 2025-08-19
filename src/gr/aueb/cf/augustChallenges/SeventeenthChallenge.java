3e8e4b73faaca42a7983be86570ebf1e5602660f
bravosgiannis <bravosgiannis@gmail.com>
2025-08-19 18:58:39 +0300

    feat: highest frequency char in a String

---
+++ src/gr/aueb/cf/augustChallenges/SeventeenthChallenge.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.augustChallenges;
+
+/*
+    Ελέγχει άν ένα String είναι παλινδρομικό, δηλαδή
+    διαβάζεται το ίδιο από την αρχή και το τέλος.
+ */
+
+public class SeventeenthChallenge {
+
+    public static void main(String[] args) {
+        String s = "daddy";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        return sb.reverse().toString().equals(s);
+    }
+}
